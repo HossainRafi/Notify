bfe1afd2547e66bb6b5820a17499b521fd2eb15c
HossainRafi <hossainrafi71@gmail.com>
2022-11-08 19:56:50 +0600

    error page done

---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+import img from "../error.gif";
+
+const Error = () => {
+  return (
+    <div className="error_page">
+      <h1>{img}</h1>
+    </div>
+  );
+};
+
+export default Error;

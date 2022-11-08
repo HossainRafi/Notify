9a35586cdffee8cb40fa6f44d5ce9d934ffccc88
HossainRafi <hossainrafi71@gmail.com>
2022-11-08 15:59:27 +0600

    user data created

---
+++ src/birthday-data.js
@@ -0,0 +1,77 @@
+export const data = [
+  {
+    id: 1,
+    name: "Babar Azam",
+    image: "https://i.ibb.co/gTQMvGx/babar-azam.jpg",
+  },
+  {
+    id: 2,
+    name: "Taskin Ahmed",
+    image: "https://i.ibb.co/By6wCZN/taskin.jpg",
+  },
+  {
+    id: 3,
+    name: "Sania Mirza",
+    image: "https://i.ibb.co/QmTZhxk/sania.jpg",
+  },
+  {
+    id: 4,
+    name: "Shahid Afridi",
+    image: "https://i.ibb.co/0m7XwVX/shahid-afridi.jpg",
+  },
+  {
+    id: 5,
+    name: "Maria Sharapova",
+    image: "https://i.ibb.co/tBVf35T/sharapova.jpg",
+  },
+  {
+    id: 6,
+    name: "Christiano Ronaldo",
+    image: "https://i.ibb.co/Rbwm2FH/ronaldo.jpg",
+  },
+  {
+    id: 7,
+    name: "Tamim Iqbal",
+    image: "https://i.ibb.co/HYh2VKc/tamim.jpg",
+  },
+  {
+    id: 8,
+    name: "Katherine Langford",
+    image: "https://i.ibb.co/Y37zHkd/katherine-langford.jpg",
+  },
+  {
+    id: 9,
+    name: "Liton Das",
+    image: "https://i.ibb.co/rvm7vxL/liton.jpg",
+  },
+  {
+    id: 10,
+    name: "Alexandra Daddario",
+    image: "https://i.ibb.co/bgyVXfz/alexandra-daddario.jpg",
+  },
+  {
+    id: 11,
+    name: "Neymer Junior",
+    image: "https://i.ibb.co/r5FDcvY/neymar.jpg",
+  },
+  {
+    id: 12,
+    name: "Leonel Messi",
+    image: "https://i.ibb.co/02rkwVr/messi.jpg",
+  },
+  {
+    id: 13,
+    name: "Mashrafee Mortaza",
+    image: "https://i.ibb.co/smCwgSx/mashrafee.jpg",
+  },
+  {
+    id: 14,
+    name: "Usain Bolt",
+    image: "https://i.ibb.co/55WVK7f/usain.jpg",
+  },
+  {
+    id: 15,
+    name: "Roger Federar",
+    image: "https://i.ibb.co/K6wbYbZ/roger-federar.jpg",
+  },
+];

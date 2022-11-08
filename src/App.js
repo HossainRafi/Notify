02361b7210d24f5926ced174d600606ea6f63b78
HossainRafi <hossainrafi71@gmail.com>
2022-11-08 19:15:51 +0600

    app.js done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-function App() {
+import React from "react";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import FrndDetails from "./components/FrndDetails";
+import FrndBirthday from "./components/FrndBirthday";
+import Error from "./components/Error";
+
+const App = () => {
   return (
-    <div>
-      <h1>Hello Everyone</h1>
-      <h1>
-        This Project Contains The Basic Setup Of A New React Project. You Need
-        To Change Only The Title & Favicon Of This Project
-      </h1>
-    </div>
+    <Router>
+      {/* <Switch> */}
+        <Route path="/" component={Home} exact />
+        <Route path="/frnddetails" component={FrndDetails} exact />
+        <Route path="/frndbirthday" component={FrndBirthday} exact />
+        <Route component={Error} />
+      {/* </Switch> */}
+    </Router>
   );
-}
+};
 
 export default App;

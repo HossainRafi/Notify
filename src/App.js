789c72ee16ad07aef5d4e443a8d6fb96146a8ded
HossainRafi <hossainrafi71@gmail.com>
2022-11-08 22:42:16 +0600

    error solved

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
 import Home from "./components/Home";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import FrndDetails from "./components/FrndDetails";
 import FrndBirthday from "./components/FrndBirthday";
 import Error from "./components/Error";
 
 const App = () => {
   return (
-    <div>
-      <h3>sdhfhdfsdhfsdhfskdjfskdjf</h3>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/frnddetails" element={<FrndDetails />} />
         <Route path="/frndbirthday" element={<FrndBirthday />} />
         <Route path="*" element={<Error />} />
       </Routes>
-    </div>
   );
 };
 
